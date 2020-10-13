ab8d657ec49a1c6aad592f8fb7e2de138cb4242a
drd-2rcl <drd.rclrd@gmail.com>
2020-10-12 22:52:49 -0300

    Create structure to orphanages page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiPlus } from 'react-icons/fi'
+
+import mapMarketingImg from '../images/map-marker.svg'
+import '../styles/pages/orphanages-map.css'
 
 export default function OrphanagesMap() {
   return (
-    <div>
-      <h1>Orfanatos</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarketingImg} alt="Happy" />
+          
+          <h2>Escolha um orfato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>SP</span>
+        </footer>
+      </aside>
+      <div></div>
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
     </div>
   )
 }
