8b816f75ee4e9ab972f5569014d57ca8fcd2483c
drd-2rcl <drd.rclrd@gmail.com>
2020-10-13 09:45:54 -0300

    Add structure to map in page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarketingImg from '../images/map-marker.svg'
 import '../styles/pages/orphanages-map.css'
@@ -21,7 +24,16 @@ export default function OrphanagesMap() {
           <span>SP</span>
         </footer>
       </aside>
-      <div></div>
+
+      <Map
+        center={[-23.5601917, -46.641581]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN }`} /> */}
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
